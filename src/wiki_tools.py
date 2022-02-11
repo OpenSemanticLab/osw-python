4322a0c2b59232ba912c55e2fbabf8dedda23792
Simon Stier <52674635+simontaurus@users.noreply.github.com>
2022-02-11 12:22:53 +0100

    add parser functions

--- src/wiki_tools.py
+++ src/wiki_tools.py
@@ -113,3 +113,80 @@ def extendPageListWithRecursiveDependencies(page_list):
         page_list.extend(dependencies)
     page_list = np.unique(page_list)
     return page_list
+
+#merges <template_name> and embedded <subtemplate_name>. text1 is dominant
+def mergeWikiText(text1, text2, template_name, subtemplate_param="", subtemplate_name=""):
+    if (text1 == text2): return text1
+    if (text1 == ""): return text2
+    if (text2 == ""): return text1
+    code1 = mwparserfromhell.parse(text1)
+    code2 = mwparserfromhell.parse(text2)
+    r1 = []
+    r2 = []
+    for template in code1.filter_templates(recursive=True):
+        if template.name.matches(template_name): t1 = template
+        if template.name.matches(subtemplate_name): r1.append(template)
+    for template in code2.filter_templates(recursive=True):
+        if template.name.matches(template_name): t2 = template
+        if template.name.matches(subtemplate_name): r2.append(template)
+    #print("T1: " + str(t1))
+    #print("T2: " + str(t2))
+    for p in t2.params:
+        if (not t1.has(p.name)):
+            t1.add(p.name, p.value)
+    if (subtemplate_param != ""): 
+        t1.remove(subtemplate_param) 
+        for rel2 in r2:
+            exists = False
+            for rel1 in r1:
+                allParamsEqual = True
+                for p in rel2.params:
+                    if (rel1.has(p.name)):
+                        if (not rel1.get(p.name).value.matches(rel2.get(p.name).value)): allParamsEqual = False
+                        #print("Compare {}:{} with :{} -> res={}".format(p.name, rel1.get(p.name).value,rel2.get(p.name).value, 
+                        #                                                rel1.get(p.name).value.matches(rel2.get(p.name).value)))
+                    else: allParamsEqual = False
+                if (allParamsEqual): exists = True
+            if not exists: r1.append(rel2)
+        r1string = ""
+        for rel1 in r1:
+            r1string += "\r\n   " + str(rel1)
+        t1.add(subtemplate_param, r1string)
+    #print("Tmerged: " + str(t1))
+    non_empty_lines = "\n".join([ll.rstrip() for ll in str(t1).splitlines() if ll.strip()])
+    return non_empty_lines
+
+def update_template_within_wikitext(text, template_text, delete=False, remove_empty_lines=False):
+    """updates the template parameters in an exisiting wiki <text> with a provided new <template_text>
+
+    Keyword arguments:
+    text -- the existing text
+    template_text -- the template text with new data
+    delete -- if true, params not defined in <template_text> get removed from <text>
+    remove_empty_lines -- if true, function will cleanup empty lines within the template code created by the nderlying mwparserfromhell lib (wanted),
+    but also within the wiki text around it (unwanted) 
+    """
+    if (template_text == text): return template_text
+    if (template_text == ""): return text
+    if (text == ""): return template_text
+    new_code = mwparserfromhell.parse(template_text)
+    new_template = new_code.filter_templates()[0]
+    existing_code = mwparserfromhell.parse(text)
+    for template in existing_code.filter_templates(recursive=True):
+        if template.name.matches(new_template.name): 
+            existing_template = template
+            break
+    for p in new_template.params:
+        if (existing_template.has(p.name)):
+            existing_template.get(p.name).value = p.value
+        else: existing_template.add(p.name, p.value)
+    if (delete):
+        for p in existing_template.params:
+            if (not new_template.has(p.name)):
+                existing_template.remove(p)
+    #print("Tmerged: \n" + str(existing_template))
+    #print("Text merged: " + str(existing_code))
+    new_text = str(existing_code);
+    #this will cleanup emptry lines within the template code (wanted), but also within the wiki text around it (unwanted) 
+    if (remove_empty_lines): new_text = "\n".join([ll.rstrip() for ll in str(new_text).splitlines() if ll.strip()])
+    return new_text

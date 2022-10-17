54869969510c436d70310de883937f2257bffddc
Simontaurus <simon.stier@gmx.de>
2022-10-17 05:50:19 +0200

    create class WtSite, WtPage

--- examples/page_manipulation.py
+++ examples/page_manipulation.py
@@ -1,51 +1,83 @@
 import sys
 sys.path.append('../')
 import src.wiki_tools as wt 
+from src.WtSite import WtSite, WtPage
 from pprint import pprint 
 
-
-wikitext_org = """{{OslTemplate:LIMS/Device/Type
-|timestamp=2022-10-10T00:00:00.000Z
-|creator=C1;C2
-|display_name=Test Term
-|label=Device Test Type with Term
-|label_lang_code=en
-|description=Some description 
-with line break
-|category=Category:OSLa444b0eeb79140d58a836a7fc6fc940a
-|relations={{OslTemplate:KB/Relation
-|property=IsRelatedTo
-|value=Term:OSL6b663c61c12d42e8be37d735dd2a869c
-}}SomeText{{OslTemplate:KB/Relation
-|property=IsRelatedTo
-|value=Term:OSL6b663c61c12d42e8be37d735dd2a869c
-}}
-}}
-=Details=
-some text
-
-{{Some/Template
-|p1=v1
-}}
-
-<br />
-{{OslTemplate:LIMS/Device/Type/Footer
-}}"""
-
-content_dict_1 = wt.create_flat_content_structure_from_wikitext(wikitext_org)
-pprint(content_dict_1)
-
-wikitext_2 = wt.get_wikitext_from_flat_content_structure(content_dict_1)
-print(wikitext_2)
-if wikitext_2 == wikitext_org: print("wikitext_2 == wikitext_org")
-else: print("wikitext_2 != wikitext_org")
-
-content_dict_3 = wt.create_flat_content_structure_from_wikitext(wikitext_2)
-pprint(content_dict_3)
-wikitext_3 = wt.get_wikitext_from_flat_content_structure(content_dict_3)
-print(wikitext_3)
-if wikitext_3 == wikitext_2: print("wikitext_3 == wikitext_2")
-else: print("wikitext_3 != wikitext_2")
-
-content_dict_3[0]['OslTemplate:LIMS/Device/Type']['display_name'] = 'NEW VALUE'
-print(wt.get_wikitext_from_flat_content_structure(content_dict_3))
+def basic_text_manipulation():
+    wikitext_org = """{{OslTemplate:LIMS/Device/Type
+    |timestamp=2022-10-10T00:00:00.000Z
+    |creator=C1;C2
+    |display_name=Test Term
+    |label=Device Test Type with Term
+    |label_lang_code=en
+    |description=Some description 
+    with line break
+    |category=Category:OSLa444b0eeb79140d58a836a7fc6fc940a
+    |relations={{OslTemplate:KB/Relation
+    |property=IsRelatedTo
+    |value=Term:OSL6b663c61c12d42e8be37d735dd2a869c
+    }}SomeText{{OslTemplate:KB/Relation
+    |property=IsRelatedTo
+    |value=Term:OSL6b663c61c12d42e8be37d735dd2a869c
+    }}
+    }}
+    =Details=
+    some text
+    
+    {{Some/Template
+    |p1=v1
+    }}
+    
+    <br />
+    {{OslTemplate:LIMS/Device/Type/Footer
+    }}"""
+    
+    content_dict_1 = wt.create_flat_content_structure_from_wikitext(wikitext_org)
+    pprint(content_dict_1)
+    
+    wikitext_2 = wt.get_wikitext_from_flat_content_structure(content_dict_1)
+    print(wikitext_2)
+    if wikitext_2 == wikitext_org: print("wikitext_2 == wikitext_org")
+    else: print("wikitext_2 != wikitext_org")
+    
+    content_dict_3 = wt.create_flat_content_structure_from_wikitext(wikitext_2)
+    pprint(content_dict_3)
+    wikitext_3 = wt.get_wikitext_from_flat_content_structure(content_dict_3)
+    print(wikitext_3)
+    if wikitext_3 == wikitext_2: print("wikitext_3 == wikitext_2")
+    else: print("wikitext_3 != wikitext_2")
+    
+    content_dict_3[0]['OslTemplate:LIMS/Device/Type']['display_name'] = 'NEW VALUE'
+    print(wt.get_wikitext_from_flat_content_structure(content_dict_3))
+    
+def mass_page_edit():
+    wtsite = WtSite.from_domain("wiki-dev.open-semantic-lab.org", "../examples/wiki-admin.pwd")    
+    #wtpage = wtsite.get_WtPage("LabNote:220601-sist-0001-ni")
+    #wtpage = wtsite.get_WtPage("testesfesefsef")
+    #wtpage.append_template("TestTemplate", {"p1": "v1"})
+    #wtpage.append_text("Some text",)
+    #wtpage.append_template("TestTemplate", {"p1": "v2"})
+    
+    #pprint(wtpage._dict)
+    
+    #res = wtpage.get_value("*.TestTemplate.p1")
+    #pprint(res)
+    #d = wtpage.set_value("*.TestTemplate.p1", "v3")
+    
+    
+    #local_id = wtpage.title.split(":")[1]
+    #wtpage.set_value("*.'OslTemplate:ELN/Entry/Header'", {"local_id" : [local_id]})
+    
+    #wtpage.update_content()
+    #pprint(wtpage._dict)
+    #wtpage.edit()
+    #print(wtpage.changed)
+            
+    wtsite.modify_search_results('semantic', '[[Category:labNote]]', 
+                                 lambda wtpage:
+                                     #print("Lambda")#wtpage.title)
+                                     wtpage.set_value("*.'OslTemplate:ELN/Entry/Header'", {"id" : [wtpage.title.split(":")[1]]}).update_content()
+                                , limit=1, comment="[bot] set id from title", log=True, dryrun=True)  
+        
+mass_page_edit()
---
+++ src/WtSite.py
@@ -0,0 +1,117 @@
+# extents mwclient.site
+
+import sys
+sys.path.append('../')
+import src.wiki_tools as wt 
+from pprint import pprint 
+import mwclient
+from jsonpath_ng.ext import parse
+
+class WtSite:
+    
+    def __init__(self, site: mwclient.Site = None):
+        if site: self._site = site
+        else: raise ValueError("Parameter 'site' is None")
+        
+    @classmethod
+    def from_domain(cls, domain: str = None, password_file: str = None):
+        site = wt.create_site_object(domain, password_file)
+        return cls(site)
+    
+    def get_WtPage(self, title: str = None):
+        wtpage = WtPage(self, title)
+        return wtpage
+    
+    def prefix_search(self, text):
+        return wt.prefix_search(self._site, text)
+    
+    def semantic_search(self, query):
+        return wt.semantic_search(self._site, query)
+
+    def modify_search_results(self, mode:str, query: str, modify_page, limit=None, comment=None, log=False, dryrun=False):
+        titles = []
+        if mode == 'prefix': titles = wt.prefix_search(self._site, query)
+        elif mode == 'semantic': titles = wt.semantic_search(self._site, query)
+        if limit: titles = titles[0:limit]
+        if log: print(f"Found: {titles}")
+        for title in titles:
+            wtpage = self.get_WtPage(title)
+            modify_page(wtpage)
+            if log: 
+                print(f"======= {title} =======")
+                print(wtpage._content + "\n")
+            if not dryrun: wtpage.edit(comment)              
+                
+            
+    
+class WtPage:
+    
+    def __init__(self, wtSite: WtSite = None, title: str = None):
+        self.wtSite = wtSite
+        self.title = title
+        
+        self._page = wtSite._site.pages[self.title]
+        self.exists = self._page.exists
+        self._original_content = ""
+        self._content = ""
+        self._dict = []
+        if self.exists: 
+            self._original_content = self._page.text()
+            self._content = self._original_content
+            self._dict = wt.create_flat_content_structure_from_wikitext(self._content)
+            
+    def append_template(self, template_name: str = None, template_params: dict = None):
+        self._dict.append({template_name: template_params})
+        return self
+
+    def append_text(self, text):
+        self._dict.append(text)
+        return self
+        
+    def get_value(self, jsonpath):
+        jsonpath_expr = parse(jsonpath)
+        res = []
+        d = dict(zip(range(len(self._dict)), self._dict)) #convert list to dict with index
+        for match in jsonpath_expr.find(d):
+            res.append(match.value)
+        return res
+    
+    def update_dict(combined: dict, update: dict) -> None:
+        for k, v in update.items():
+            if isinstance(v, dict):
+                WtPage.combine_into(v, combined.setdefault(k, {}))
+            else:
+                combined[k] = v
+    
+    def set_value(self, jsonpath_match, value, replace = False):
+        jsonpath_expr = parse(jsonpath_match)
+        d = dict(zip(range(len(self._dict)), self._dict)) #convert list to dict with index
+        #if create: jsonpath_expr.update_or_create(d, value)
+        #else: jsonpath_expr.update(d, value)
+        matches = jsonpath_expr.find(d)
+        for match in matches:
+            print(match.full_path)
+            #pprint(value)
+            if not replace: 
+                WtPage.update_dict(match.value, value)
+                value = match.value
+            #pprint(value)
+            match.full_path.update_or_create(d, value)
+        self._dict = list(d.values()) #convert dict with index to list
+        return self
+
+    def update_content(self):
+        self._content = wt.get_wikitext_from_flat_content_structure(self._dict)
+        self.changed = self._original_content != self._content
+        return self
+    
+    def edit(self, comment: str = None):
+        if not comment: comment = "[bot] update of page content"
+        if self.changed: self._page.edit(self._content, comment);
+
+ 
+        
+    
+        
+    
+

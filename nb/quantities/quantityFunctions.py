1203d865420fafc27d4e2a2864b1b026557b9508
Andreas Räder <andreas.raeder@isc.fraunhofer.de>
2023-03-03 17:49:57 +0100

    fetch and analyze quantities completed

--- nb/quantities/quantityFunctions.py
+++ nb/quantities/quantityFunctions.py
@@ -1,9 +1,18 @@
 #1: declare functions
-from SPARQLWrapper import SPARQLWrapper, POST, BASIC, JSON
 import json
+import os
+from SPARQLWrapper import SPARQLWrapper, POST, BASIC, JSON
+from pathlib import Path
 from pprint import pprint
+from utils import dict_to_json, json_to_dict
 
+# call this class to fetch quantites
 class QuantityFetcher():
+    def __init__(self):
+        print("\nfetching quantities...")
+        self.fetchQuantities()
+        print("\nquantities fetched\n")
+                
     def sparqlQuery(self, endpoint, query):
         sparql = SPARQLWrapper(endpoint)
         sparql.setMethod(POST)
@@ -255,7 +264,74 @@ class QuantityFetcher():
         #if ucum != result: print("Normalize '{}' to '{}'".format(ucum, result)) 
         return result
     #nomalize_ucum("kg/(m.s2)") 
-            
+    
+    def fetchQuantities(self):
+        limit = 999999999
+        _qdict = self.getQudtTypeDict('QuantityKind', limit)
+        _udict = self.getQudtTypeDict('Unit', limit)
+        dSiDict = self.getQudtTypeDict('QuantityKindDimensionVector_SI', limit)
+        dIsoDict = self.getQudtTypeDict('QuantityKindDimensionVector_ISO', limit)
+        dImpDict = self.getQudtTypeDict('QuantityKindDimensionVector_Imperial', limit)
+        dCgsDict = self.getQudtTypeDict('QuantityKindDimensionVector_CGS', limit)
+        _ddict = {**dImpDict, **dCgsDict, **dIsoDict, **dSiDict} #merge dicts
+        dict_to_json(_qdict, '_qdict')
+        dict_to_json(_udict, '_udict')
+        dict_to_json(dSiDict, 'dSiDict')
+        dict_to_json(dIsoDict, 'dIsoDict')
+        dict_to_json(dImpDict, 'dImpDict')
+        dict_to_json(dCgsDict, 'dCgsDict')
+        dict_to_json(_ddict, '_ddict')
+        #for q in qdict: print(q)
+        #for u in udict: print(u)
+        #for d in ddict: print(d)
+        print(len(_qdict))
+        print(len(_udict))
+        print(len(_ddict))
+
+        #wdict1 = get_sparql_triplets("https://query.wikidata.org/sparql", "?subject <http://www.wikidata.org/prop/direct/P279>+ <http://www.wikidata.org/entity/Q107715>.") #195656 triplets found
+        #wdict2 = get_sparql_triplets("https://query.wikidata.org/sparql", "?subject <http://www.wikidata.org/prop/direct/P8393> ?qudtid.") #48411 triplets found
+        #wdict = {**wdict1, **wdict2} #merge dicts, 3966 results
+        wdict = self.get_sparql_triplets("https://query.wikidata.org/sparql", "?subject <http://www.wikidata.org/prop/direct/P279>*/<http://www.wikidata.org/prop/direct/P8393> ?qudtid.") #quantities with qudt ref and their subq, 2484 results
+        dict_to_json(wdict, 'wdict')
+        print(len(wdict))
+        #only subquantities
+        #wdict = get_sparql_triplets("https://query.wikidata.org/sparql", "?subject <http://www.wikidata.org/prop/direct/P279>/<http://www.wikidata.org/prop/direct/P279>+ <http://www.wikidata.org/entity/Q107715>.")
+        #print(len(wdict))
+        #wstdict1 = get_sparql_wikidata_statement_triplets("https://query.wikidata.org/sparql", "?subject <http://www.wikidata.org/prop/direct/P279>+ <http://www.wikidata.org/entity/Q107715>.") #get statement object, 131347 triplets
+        #wstdict2 = get_sparql_wikidata_statement_triplets("https://query.wikidata.org/sparql", "?subject <http://www.wikidata.org/prop/direct/P8393> ?qudtid.") #get statement object, 37775 triplets
+        #wstdict = {**wstdict1, **wstdict2} #merge dicts, 3966 results
+        wstdict = self.get_sparql_wikidata_statement_triplets("https://query.wikidata.org/sparql", "?subject <http://www.wikidata.org/prop/direct/P279>*/<http://www.wikidata.org/prop/direct/P8393> ?qudtid.") #get statement object
+        dict_to_json(wstdict, 'wstdict')
+        print(len(wstdict))
+        for st in wstdict:
+            for p in wstdict[st]:
+                o = wstdict[st][p]
+                wdict[st][p] = o #replace statement with object
+        wudict1 = self.get_sparql_triplets("https://query.wikidata.org/sparql", "?subject <http://www.wikidata.org/prop/direct/P31> <http://www.wikidata.org/entity/Q47574>.") #instance of unit of measurement: 1340 results
+        wudict2 = self.get_sparql_triplets("https://query.wikidata.org/sparql", "?subject <http://www.wikidata.org/prop/direct/P5061> ?symbol.") #has unit symbol: 2230 results
+        wudict3 = self.get_sparql_triplets("https://query.wikidata.org/sparql", "?quantity <http://www.wikidata.org/prop/P8111>/<http://www.wikidata.org/prop/statement/P8111> ?subject.") #is recommended unit of measurement of: 294 results
+        wudict4 = self.get_sparql_triplets("https://query.wikidata.org/sparql", "?subject <http://www.wikidata.org/prop/P111>/<http://www.wikidata.org/prop/statement/P111> ?quantity.") #has measured quantity: 3278 results
+        wudict = {**wudict1, **wudict2, **wudict3, **wudict4} #merge dicts, 3966 results
+        len(wudict)
+        dict_to_json(wudict, 'wudict')
+        
+        #dbpdict = get_sparql_triplets("https://dbpedia.org/sparql/", "?subject <http://purl.org/dc/terms/subject> <http://dbpedia.org/resource/Category:Physical_quantities>.", limit=11000)
+        #dbpdict = get_sparql_triplets("https://dbpedia.org/sparql/", "?subject <http://purl.org/dc/terms/subject>/<http://www.w3.org/2004/02/skos/core#broader> <http://dbpedia.org/resource/Category:Quantity>.")
+        #endpoint is limited to 10000 results so we have to restrict the request to the relevant property
+        dbpdict = self.get_sparql_triplets("https://dbpedia.org/sparql/", "?subject <http://purl.org/dc/terms/subject>/<http://www.w3.org/2004/02/skos/core#broader> <http://dbpedia.org/resource/Category:Quantity>.", tpredicate="http://www.w3.org/2002/07/owl#sameAs")
+        print(len(dbpdict))
+        dict_to_json(dbpdict, 'dbpdict')
+    
+
+class Quantities():
+    def __init__(self, wdict):
+        self.wdict = wdict
+        
+    def remove_wikidata_superclass(self, w, sup):  
+        if sup in self.wdict[w]['http://www.wikidata.org/prop/P279']: 
+            self.wdict[w]['http://www.wikidata.org/prop/P279'].remove(sup) 
+            print("remove_wikidata_superclass: ", sup)
+        
     def set_wikidata_base_and_super_quantities(self, wId, ignore = []):
         w = self.wdict[wId]
         #print(json.dumps(q))
@@ -277,6 +353,53 @@ class QuantityFetcher():
         self.wdict[wId]['subquantity_of'] = subquantity_of 
         if (len(base_quantity) > 1): print("WARNING: More than one base quantity for {}: {}".format(wId, base_quantity))
         return base_quantity
+    
+    def analyzeWikidata(self):
+        print("\nanalyzing wikidata...")
+        w_base_q = []
+        no_unit = []
+        no_dim = []
+        for w in self.wdict:
+            #if 'http://www.wikidata.org/entity/Q107715' in wdict[w]['http://www.wikidata.org/prop/direct/P279']:
+            #    w_base_q.append(w)
+            sub_q = False
+            for sup in self.wdict[w]['http://www.wikidata.org/prop/direct/P279']:
+                if sup in self.wdict:
+                    sub_q = True
+                    break
+            if not sub_q: w_base_q.append(w)
+            if 'http://www.wikidata.org/prop/direct/P4020' not in self.wdict[w]: no_dim.append(w)
+            else: 
+                if 'http://www.wikidata.org/prop/direct/P8111' not in self.wdict[w] and self.wdict[w]['http://www.wikidata.org/prop/direct/P4020'] != "1":
+                    no_unit.append(w)
+                
+        print("w_base_q: ", len(w_base_q))    
+        print("no_unit: ", len(no_unit))      
+        print("no_dim: ", len(no_dim))      
+        #for w in w_base_q:
+        #    print(getLangLabel(wdict[w]["http://www.w3.org/2000/01/rdf-schema#label"], 'en'))
+        ignore_list = []
+        ignore_list.append('http://www.wikidata.org/entity/Q181175') #scalar quantity
+        ignore_list.append('http://www.wikidata.org/entity/Q2672914') #vector quantity
+        self.remove_wikidata_superclass('http://www.wikidata.org/entity/Q589446', 'http://www.wikidata.org/entity/Q3686031') #mass concentration is not subclass of amount concentration
+        self.remove_wikidata_superclass('http://www.wikidata.org/entity/Q76378758', 'http://www.wikidata.org/entity/Q3686031') #mass concentration of water is not subclass of amount concentration
+        self.remove_wikidata_superclass('http://www.wikidata.org/entity/Q13824', 'http://www.wikidata.org/entity/Q11465') #phase speed is not a velocity
+        self.remove_wikidata_superclass('http://www.wikidata.org/entity/Q86508881', 'http://www.wikidata.org/entity/Q11465') #sound speed is not a velocity
+        self.remove_wikidata_superclass('http://www.wikidata.org/entity/Q86508881', 'http://www.wikidata.org/entity/Q217361') #sound speed is not a group velocity Q217361
+        self.remove_wikidata_superclass('http://www.wikidata.org/entity/Q1097654', 'http://www.wikidata.org/entity/Q11379') #rest mass is not an energy
+        self.remove_wikidata_superclass('http://www.wikidata.org/entity/Q4128476', 'http://www.wikidata.org/entity/Q25288') #birth weight is a mass, not a force
+        self.remove_wikidata_superclass('http://www.wikidata.org/entity/Q6795900', 'http://www.wikidata.org/entity/Q25288') #landing weight is a mass, not a force
+        self.remove_wikidata_superclass('http://www.wikidata.org/entity/Q6795910', 'http://www.wikidata.org/entity/Q25288') #maximum ramp weight is a mass, not a force
+        self.remove_wikidata_superclass('http://www.wikidata.org/entity/Q16815754', 'http://www.wikidata.org/entity/Q25288') #operating weight is a mass, not a force 
+        self.remove_wikidata_superclass('http://www.wikidata.org/entity/Q55435676', 'http://www.wikidata.org/entity/Q275157') #fasting serum glucose level is a mass concentration, blood glucose is a amount concentration
+        ignore_list.append('http://www.wikidata.org/entity/Q111811675') #area dublicate
+        self.remove_wikidata_superclass('http://www.wikidata.org/entity/Q11500', 'http://www.wikidata.org/entity/Q111811675') #area is not subclass of dublicated area class (recursion)
+        counter = 0
+        for w in self.wdict:
+            self.set_wikidata_base_and_super_quantities(w, ignore_list)
+            counter += 1
+            
+        print("set_wikidata_base_and_super_quantities counter: ", counter, "\n")
 
-    def remove_wikidata_superclass(self, w, sup):
-        if sup in self.wdict[w]['http://www.wikidata.org/prop/P279']: self.wdict[w]['http://www.wikidata.org/prop/P279'].remove(sup) 
+            
+        
--- nb/quantities/utils.py
+++ nb/quantities/utils.py
@@ -3,7 +3,8 @@ import pandas as pd
 from pathlib import Path
 import json
 
-def dict_to_json(dictionary, name_of_json, optional_orient=None, optional_path=None):
+# problem with escaping of uri's, reimplementation of dict_to_json below
+def dictdf_to_json(dictionary={}, name_of_json="sample", optional_orient=None, optional_path=None):
     if optional_orient is None:
         df_json = pd.DataFrame.from_dict(dictionary)
     else:
@@ -15,4 +16,30 @@ def dict_to_json(dictionary, name_of_json, optional_orient=None, optional_path=N
         Path(optional_path)    
     # filepath_json.parent.mkdir(parents=True, exist_ok=True)  
     df_json.to_json(filepath_json)  
-    
+
+# export dictionary as json file
+def dict_to_json(dictionary={}, name_of_json="sample", optional_path=None):
+    if optional_path is None: 
+        filepath_json = os.path.join(os.path.dirname(os.path.abspath(__file__)), "data", f"{name_of_json}.json")
+        with open(filepath_json, "w") as outfile:
+            json.dump(dictionary, outfile)
+            print(name_of_json, "created as json using filepath: \n", filepath_json)
+    else: 
+        optional_filepath = Path(optional_path)    
+        with open(optional_filepath, "w") as outfile:
+            json.dump(dictionary, outfile)
+            print(name_of_json, "created as json using optional filepath: \n", optional_filepath)
+ 
+# import data from json file as dictonary
+def json_to_dict(name_of_json="sample", optional_path=None):
+    if optional_path is None: 
+        filepath_json = os.path.join(os.path.dirname(os.path.abspath(__file__)), "data", f"{name_of_json}.json")
+        print(filepath_json)
+        with open(filepath_json) as json_file:
+            print(name_of_json, "loaded using filepath:\n", filepath_json)
+            return json.load(json_file)
+    else:
+        optional_filepath = Path(optional_path)
+        with open(optional_path) as json_file:
+            print(name_of_json, "loaded using optional filepath:\n", optional_filepath)
+            return json.load(json_file)

eca0eba93eef5043e1f44d2b129f00942ae6d352
Lukas Gold <41064839+LukasGold@users.noreply.github.com>
2023-03-20 09:12:23 +0100

    Fix trailing white spaces

--- src/osw/model/page_package.py
+++ src/osw/model/page_package.py
@@ -177,12 +177,12 @@ class PagePackageConfig(BaseModel):
     include_files: Optional[bool] = True
 
     def __init__(self, **data):
-        """Originally, the dataclass.__post_init_() method was used here. 
+        """Originally, the dataclass.__post_init_() method was used here.
         Pydantic does not implement anything similar yet. According to
         https://github.com/pydantic/pydantic/issues/1729#issuecomment-1300576214
-        no __post_init__ method will be available until Pydantic v2. Then an 
-        analogous function will be implemented and any in a BaseModel class defined 
-        method named 'model_post_init(self, **kwargs)' will be called after 
+        no __post_init__ method will be available until Pydantic v2. Then an
+        analogous function will be implemented and any in a BaseModel class defined
+        method named 'model_post_init(self, **kwargs)' will be called after
         init."""
         super().__init__(self, **data)
         if self.content_path == "":

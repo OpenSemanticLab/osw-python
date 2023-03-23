d8a49aed5a118b537cbf5f4f1ff86ead1c6bbc01
Lukas Gold <lukas.gold@isc.fraunhofer.de>
2023-03-23 14:32:48 +0100

    Implemented an alternative cast method discriminating None values

--- src/osw/model/entity.py
+++ src/osw/model/entity.py
@@ -42,6 +42,23 @@ class OswBaseModel(BaseModel):
     def cast(self, cls: Type[T]) -> T:
         return cls(**self.dict())
 
+    def cast_none_to_default(self, cls: Type[T]) -> T:
+        """Casting self into target class. If the passed attribute is None or solely
+        includes None values, the attribute is not passed to the instance of the
+        target class, which will then fall back to the default."""
+        def test_if_empty_list_or_none(obj) -> bool:
+            if obj is None:
+                return True
+            elif isinstance(obj, list):
+                if len(obj) == 0:
+                    return True
+                elif len([item for item in obj if item is not None]) > 0:
+                    return False
+
+        return cls(**{
+            k: v for k, v in self.dict().items() if not test_if_empty_list_or_none(v)
+        })
+
 
 @_basemodel_decorator
 class Label(OswBaseModel):

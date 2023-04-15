30d9037fade307183ee8d8502418e7a62c345be5
SimonTaurus <simon.stier@gmx.de>
2023-04-15 12:38:51 +0200

    test: add db integration test

---
+++ tests/integration/db_test.py
@@ -0,0 +1,60 @@
+import mwclient
+
+import osw.model.entity as model
+from osw.auth import CredentialManager
+from osw.core import OSW
+from osw.wtsite import WtSite
+
+# run with: tox -e test -- --wiki_domain domain --wiki_username user --wiki_password pass --db_username user --db_password pass
+
+
+def test_connect_and_query(
+    wiki_domain, wiki_username, wiki_password, db_username, db_password
+):
+    site = mwclient.Site(host=wiki_domain)
+    site.login(username=wiki_username, password=wiki_password)
+    wtsite = WtSite(site)
+    osw = OSW(site=wtsite)
+
+    osw.fetch_schema(
+        OSW.FetchSchemaParam(
+            schema_title="Category:OSW02590972aeba46d7864ed492c0c11384",  # Host
+            mode="replace",
+        )
+    )
+    osw.fetch_schema(
+        OSW.FetchSchemaParam(
+            schema_title="Category:OSWacdb001c926c46b998af3e645d36b13f",
+            # DatabaseServer
+            mode="append",
+        )
+    )
+    osw.fetch_schema(
+        OSW.FetchSchemaParam(
+            schema_title="Category:OSW51ad0d1716254c77a2b7a03217f23b43",  # Database
+            mode="append",
+        )
+    )
+    osw.fetch_schema(
+        OSW.FetchSchemaParam(
+            schema_title="Category:OSW33c7c3a4076a4a58b20d818c162e84e6",  # DatabaseType
+            mode="append",
+        )
+    )
+
+    # make sure to import controllers after updating the model (ignore linter warning)
+    import osw.controller as controller  # noqa: E402
+
+    # load database definition
+    db = osw.load_entity("Item:OSWb8cc7705e17c47b19331fdb045bfbca8")  # postgres
+    db = db.cast(model.Database)
+
+    # cast into controller and execute function
+    db = db.cast(controller.DatabaseController)
+    db.connect(
+        controller.DatabaseController.ConnectionConfig(
+            cm=CredentialManager.Credential(username=db_username, password=db_password),
+            osw=osw,
+        )
+    )
+    db.execute("SELECT * FROM public.sensors ORDER BY sensor_id ASC")

f006594e28d5a05903100381a7ce634e5864b3dd
Lukas Gold <lukas.gold@isc.fraunhofer.de>
2023-05-11 15:07:29 +0200

    Rework for readability and use in a tutorial

--- examples/create_page_package.py
+++ examples/create_page_package.py
@@ -1,63 +1,89 @@
-import os
+from pathlib import Path
 
 import osw.model.page_package as package
 from osw.wtsite import WtSite
 
-pwd_file_path = os.path.join(
-    os.path.dirname(os.path.abspath(__file__)), "accounts.pwd.yaml"
-)
+# Specify the path to the credentials file
+pwd_file_path = Path(__file__).parent / "accounts.pwd.yaml"
 
-wtsite = WtSite.from_domain("wiki-dev.open-semantic-lab.org", pwd_file_path)
+# Specify the domain of the OSW/OSL instance to load pages from
+domain = "wiki-dev.open-semantic-lab.org"
 
-package_repo_org = "OpenSemanticWorld-Packages"
-package_repo = "world.opensemantic.core"
-package_id = "world.opensemantic.core"
+
+# Provide information on the package to be created
+# Package name
 package_name = "OSW Core"
+# Package repository name - usually the GitHub repository name
+package_repo = "world.opensemantic.core"
+# Package ID - usually the same as package_repo
+package_id = "world.opensemantic.core"
+# Package subdirectory - usually resembling parts of the package name
 package_subdir = "core"
+# Package branch - usually "main"
 package_branch = "main"
-
-working_dir = os.path.join(
-    os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))),
-    "packages",
-    package_repo,
+# (GitHub) Organization hosting the package repository
+package_repo_org = "OpenSemanticWorld-Packages"
+# Provide a description
+package_description = (
+    "Provides core functionalities of OpenSemanticWorld / OpenSemanticLab"
 )
+# Specify version
+package_version = "0.2.1"
+# Authors
+authors = ["Simon Stier", "Lukas Gold"]
+# Publisher
+publisher = "OpenSemanticWorld"
+# List the full page titles of the pages to be included in the package
+# You can include a comment in the same line, stating the page label
+page_titles = [
+    "Module:Lustache",
+    "Module:Lustache/Context",
+    "Module:Lustache/Renderer",
+    "Module:Lustache/Scanner",
+    "Module:Entity",
+    "Module:MwJson",
+    "JsonSchema:Label",
+    "JsonSchema:Description",
+    "Category:Category",
+    "Category:Entity",
+    "Category:Item",
+    "Template:Helper/UI/Tiles/Grid",
+    "Template:Helper/UI/Tiles/Tile",
+]
 
+
+# Specify the path to the working directory - where the package is stored on disk
+working_dir = Path(__file__).parents[2] / "packages" / package_repo
+
+
+# Usually, you won't need to change anything below this line
+# ----------------------------------------------------------------------
+# Create a WtSite instance to load pages from the specified domain
+wtsite = WtSite.from_domain(domain, pwd_file_path)
+# Create a PagePackageBundle instance
 bundle = package.PagePackageBundle(
-    publisher="OpenSemanticWorld",
-    author=["Simon Stier"],
+    publisher=publisher,
+    author=authors,
     language="en",
     publisherURL=f"https://github.com/{package_repo_org}/{package_repo}",
     packages={
         f"{package_name}": package.PagePackage(
             globalID=f"{package_id}",
             label=package_name,
-            version="0.2.1",
-            description="Provides core functionalities of OpenSemanticWorld / OpenSemanticLab",
-            baseURL=f"https://raw.githubusercontent.com/{package_repo_org}/{package_repo}/{package_branch}/{package_subdir}/",
+            version=package_version,
+            description=package_description,
+            baseURL=f"https://raw.githubusercontent.com/{package_repo_org}/"
+            f"{package_repo}/{package_branch}/{package_subdir}/",
         )
     },
 )
-
-wtsite.create_page_package(
-    package.PagePackageConfig(
-        name=package_name,
-        config_path=os.path.join(working_dir, "packages.json"),
-        content_path=os.path.join(working_dir, package_subdir),
-        bundle=bundle,
-        titles=[
-            "Module:Lustache",
-            "Module:Lustache/Context",
-            "Module:Lustache/Renderer",
-            "Module:Lustache/Scanner",
-            "Module:Entity",
-            "Module:MwJson",
-            "JsonSchema:Label",
-            "JsonSchema:Description",
-            "Category:Category",
-            "Category:Entity",
-            "Category:Item",
-            "Template:Helper/UI/Tiles/Grid",
-            "Template:Helper/UI/Tiles/Tile",
-        ],
-    )
+# Create a PagePackageConfig instance
+config = package.PagePackageConfig(
+    name=package_name,
+    config_path=working_dir / "packages.json",
+    content_path=working_dir / package_subdir,
+    bundle=bundle,
+    titles=page_titles,
 )
+# Create the page package in the working directory
+wtsite.create_page_package(config=config)

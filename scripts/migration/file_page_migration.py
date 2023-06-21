812ef2c27491aa12add8a1184e135b2c37f0e106
Lukas Gold <lukas.gold@isc.fraunhofer.de>
2023-06-21 13:12:29 +0200

    Added expected_groups to the RegExPatternExtended instances for testing

--- scripts/migration/file_page_migration.py
+++ scripts/migration/file_page_migration.py
@@ -59,6 +59,7 @@ REGEX_PATTERN_LIST = [
         r"(\.[a-zA-Z0-9]*[a-zA-Z]+[a-zA-Z0-9]*)",
         group_keys=["Label", "Suffix(es)"],  # ["OSW-ID", "Suffix(es)"]
         example_str="File:OSW11a53cdfbdc24524bf8ac435cbf65d9d.svg",
+        expected_groups=["OSW11a53cdfbdc24524bf8ac435cbf65d9d", ".svg"],
     ),
     RegExPatternExtended(
         description="File page full page title (old format) with OSW-ID and two "
@@ -69,6 +70,12 @@ REGEX_PATTERN_LIST = [
         group_keys=["Namespace", "OSW-ID", "Label", "Suffix(es)"],
         example_str="File:LabNote-OSL9568a776dcdf4b7e86f462993df12059-"
         "spreadsheet-01.luckysheet.json",
+        expected_groups=[
+            "LabNote",
+            "OSL9568a776dcdf4b7e86f462993df12059",
+            "spreadsheet-01",
+            ".luckysheet.json",
+        ],
     ),
     RegExPatternExtended(
         description="File page full page title (old format) with OSW-ID and one suffix",
@@ -76,6 +83,12 @@ REGEX_PATTERN_LIST = [
         r"(\.[a-zA-Z0-9]*[a-zA-Z]+[a-zA-Z0-9]*)",
         group_keys=["Namespace", "OSW-ID", "Label", "Suffix(es)"],
         example_str="File:LabNote-OSL9ee686dbcc654f4abeaf5f38b8487885-sketch-05.svg",
+        expected_groups=[
+            "LabNote",
+            "OSL9ee686dbcc654f4abeaf5f38b8487885",
+            "sketch-05",
+            ".svg",
+        ],
     ),
     RegExPatternExtended(
         description="File page full page title (old format) with namespace, ISC-ID and "
@@ -85,6 +98,7 @@ REGEX_PATTERN_LIST = [
         r"\.[a-zA-Z0-9]*[a-zA-Z]+[a-zA-Z0-9]*)",
         group_keys=["Namespace", "ISC-ID", "Label", "Suffix(es)"],
         example_str="File:LabNote-220304-test-0002-ni-diagram-01.drawio.svg",
+        expected_groups=["LabNote", "220304-test-0002", "ni-diagram-01", ".drawio.svg"],
     ),
     RegExPatternExtended(
         description="File page full page title (old format) with namespace, ISC-ID and "
@@ -93,6 +107,12 @@ REGEX_PATTERN_LIST = [
         r"(\.[a-zA-Z0-9]*[a-zA-Z]+[a-zA-Z0-9]*)",
         group_keys=["Namespace", "ISC-ID", "Label", "Suffix(es)"],
         example_str="File:LabNote-220305-sist-0001-ni-sketch-furnace-black.svg",
+        expected_groups=[
+            "LabNote",
+            "220305-sist-0001",
+            "ni-sketch-furnace-black",
+            ".svg",
+        ],
     ),
     RegExPatternExtended(
         description="File page full page title (old format) with ISC-ID and two "
@@ -102,6 +122,7 @@ REGEX_PATTERN_LIST = [
         r"\.[a-zA-Z0-9]*[a-zA-Z]+[a-zA-Z0-9]*)",
         group_keys=["ISC-ID", "Label", "Suffix(es)"],
         example_str="File:211104-sist-0003-n stamped 20211116080457.pdf.ots",
+        expected_groups=["211104-sist-0003", "n stamped 20211116080457", ".pdf.ots"],
     ),
     RegExPatternExtended(
         description="File page full page title (old format) with ISC-ID and "
@@ -110,6 +131,7 @@ REGEX_PATTERN_LIST = [
         r"(\.[a-zA-Z0-9]*[a-zA-Z]+[a-zA-Z0-9]*)",
         group_keys=["ISC-ID", "Label", "Suffix(es)"],
         example_str="File:220304-test-0003-ni stamped 20220304155047.pdf",
+        expected_groups=["220304-test-0003", "ni stamped 20220304155047", ".pdf"],
     ),
     RegExPatternExtended(
         description="File page full page title with two suffixes",
@@ -117,12 +139,17 @@ REGEX_PATTERN_LIST = [
         r"\.[a-zA-Z0-9]*[a-zA-Z]+[a-zA-Z0-9]*)",
         group_keys=["Label", "Suffix(es)"],
         example_str="File:OpenSemanticLab-vertical-process-abstraction-en.drawio.svg",
+        expected_groups=[
+            "OpenSemanticLab-vertical-process-abstraction-en",
+            ".drawio.svg",
+        ],
     ),
     RegExPatternExtended(
         description="File page full page title with one suffix",
         pattern=r"File:(.+)(\.[a-zA-Z0-9]*[a-zA-Z]+[a-zA-Z0-9]*)",
         group_keys=["Label", "Suffix(es)"],
         example_str="File:ISC-Digital-1440x448px.jpg",
+        expected_groups=["ISC-Digital-1440x448px", ".jpg"],
     ),
 ]
 REGEX_PATTERN = {rep.description: rep.dict() for rep in REGEX_PATTERN_LIST}

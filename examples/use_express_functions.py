16f35c610e5cb18744efc62a00af6397c2f073ad
Lukas Gold <lukas.gold@isc.fraunhofer.de>
2024-02-26 18:05:58 +0100

    Implementing context manager compatible osw_download_file

--- examples/use_express_functions.py
+++ examples/use_express_functions.py
@@ -2,11 +2,14 @@ from osw.express import OswExpress, credentials_fp_default, osw_download_file, o
 
 # (Optional) Set the default credentials filepath to desired location. Otherwise,
 # it will use the default location (current working directory)
-credentials_fp_default.set_default(r"C:\Users\gold\ownCloud\Personal\accounts.pwd.yaml")
+# credentials_fp_default.set_default(r"C:\Users\gold\ownCloud\Personal\accounts.pwd.yaml")
+
+# Check setting
+print(f"Credentials loaded from '{str(credentials_fp_default)}")
 
 # Create an OswExpress object
-# domain = "wiki-dev.open-semantic-lab.org"
-domain = "arkeve.test.digital.isc.fraunhofer.de"
+domain = "wiki-dev.open-semantic-lab.org"
+# domain = "arkeve.test.digital.isc.fraunhofer.de"
 osw = OswExpress(domain=domain)
 
 # Download a file from an OSW instance and save it to a local file
@@ -16,15 +19,19 @@ local_file = osw_download_file(
     overwrite=True,
 )
 local_file_path = local_file.path
-# Open the file with context manager directly from an OSW instance
+local_file.close()  # required to release the file lock
+
+# Open a file with context manager directly from an OSW instance
 with osw_open(
     "https://wiki-dev.open-semantic-lab.org/wiki/"
-    "File:OSWac9224e1a280449dba71d945b1581d57.txt"
+    "File:OSWbd78f7a00597492e956e152736de4a05.txt"
 ) as file:
     content = file.read()
-# Doesn't work yet:
-# with osw_download_file(
-#         "File:OSWac9224e1a280449dba71d945b1581d57.txt", domain=domain,
-#         overwrite=True
-# ) as file:
-#     content2 = file.read()
+
+# Using a context manager
+with osw_download_file(
+    "File:OSWac9224e1a280449dba71d945b1581d57.txt",
+    domain=domain,
+    overwrite=True,
+) as file:
+    content2 = file.read()

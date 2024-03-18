50cf8a6e8606bfa1c8554da3053acb3f87d66c59
Lukas Gold <lukas.gold@isc.fraunhofer.de>
2024-03-18 16:49:11 +0100

    changes default behavior to persist credentials + treating all cases, incl. cred_filepath being a list

--- src/osw/auth.py
+++ src/osw/auth.py
@@ -10,6 +10,8 @@ from pydantic.v1 import FilePath, PrivateAttr
 
 from osw.model.static import OswBaseModel
 
+CREDENTIALS_FN_DEFAULT = "credentials.pwd.yaml"
+
 
 class CredentialManager(OswBaseModel):
     """Handles credentials"""
@@ -37,7 +39,8 @@ class CredentialManager(OswBaseModel):
         """the users password"""
 
     class OAuth1Credential(BaseCredential):
-        """OAuth1 credentials. See https://requests-oauthlib.readthedocs.io/en/latest/oauth1_workflow.html"""
+        """OAuth1 credentials. See
+        https://requests-oauthlib.readthedocs.io/en/latest/oauth1_workflow.html"""
 
         consumer_token: str
         """consumer token"""
@@ -72,6 +75,12 @@ class CredentialManager(OswBaseModel):
         fallback: Optional[CredentialManager.CredentialFallback] = "none"
         """The fallback strategy if no credential was found for the given origin"""
 
+    def __init__(self, **data):
+        super().__init__(**data)
+        if self.cred_filepath:
+            if not isinstance(self.cred_filepath, list):
+                self.cred_filepath = list(self.cred_filepath)
+
     def get_credential(self, config: CredentialConfig) -> BaseCredential:
         """Reads credentials from a yaml file or the in memory store
 
@@ -90,8 +99,6 @@ class CredentialManager(OswBaseModel):
         _file_credentials: List[CredentialManager.BaseCredential] = []
         if self.cred_filepath:
             filepaths = self.cred_filepath
-            if type(filepaths) is not list:
-                filepaths = [filepaths]
 
             for filepath in filepaths:
                 if filepath != "":
@@ -153,6 +160,9 @@ class CredentialManager(OswBaseModel):
                 cred = CredentialManager.UserPwdCredential(
                     username=username, password=password, iri=config.iri
                 )
+                self.add_credential(cred)
+                if self.cred_filepath:
+                    self.save_credentials_to_file()
         return cred
 
     def add_credential(self, cred: BaseCredential):
@@ -197,55 +207,90 @@ class CredentialManager(OswBaseModel):
             True if a credential exists for the given iri
         """
         if self.cred_filepath:
-            if not self.cred_filepath.exists():
-                return False
-            filepaths = self.cred_filepath
-            if type(filepaths) is not list:
-                filepaths = [filepaths]
-
-            for filepath in filepaths:
-                if filepath != "":
-                    with open(filepath, "r") as stream:
-                        try:
-                            accounts = yaml.safe_load(stream)
-                            if accounts is None:  # Catch empty file
-                                continue
-                            for iri_ in accounts.keys():
-                                if iri_ == iri:
-                                    return True
-                        except yaml.YAMLError as exc:
-                            print(exc)
+            for fp in self.cred_filepath:
+                if fp != "":
+                    if Path(fp).exists():
+                        with open(fp, "r") as stream:
+                            try:
+                                accounts = yaml.safe_load(stream)
+                                if accounts is None:  # Catch empty file
+                                    continue
+                                for iri_ in accounts.keys():
+                                    if iri_ == iri:
+                                        return True
+                            except yaml.YAMLError as exc:
+                                print(exc)
         return False
 
     def save_credentials_to_file(
         self,
         filepath: Union[str, FilePath] = None,
+        set_cred_filepath: bool = False,
     ):
-        """saves the in memory credentials to a file
+        """Saves the in memory credentials to a file
 
         Parameters
         ----------
         filepath
             The filepath to save the credentials to. If None, the filepath specified
-            in the CredentialManager is used. If specified, the cred_filepath of the
-            CredentialManager is updated.
+            in the CredentialManager is used.  If cred_filepath and filepath are None,
+            the default path is used. If the file does not exist, it is created.
+        set_cred_filepath
+            If True, the cred_filepath is set to the given filepath. If False, the
+            cred_filepath of the CredentialManager is not changed.
         """
+        filepath_ = [filepath]
         if filepath is None:
-            filepath = self.cred_filepath
-        else:
-            self.cred_filepath = filepath
-        file = Path(filepath)
-        if not file.parent.exists():
-            file.parent.mkdir(parents=True)
-        data = {}
-        if file.exists():
-            data = yaml.safe_load(file.read_text())
-            if data is None:  # Catch empty file
-                data = {}
-        for cred in self._credentials:
-            data[cred.iri] = cred.dict(exclude={"iri"})
-        with open(filepath, "w") as stream:
-            yaml.dump(data, stream)
+            filepath_ = self.cred_filepath
+            if self.cred_filepath is None:
+                filepath_ = [Path.cwd() / CREDENTIALS_FN_DEFAULT]
+        if set_cred_filepath:
+            self.cred_filepath = filepath_
+        for fp in filepath_:
+            file = Path(fp)
+            if not file.parent.exists():
+                file.parent.mkdir(parents=True)
+            data = {}
+            file_already_exists = file.exists()
+            if file_already_exists:
+                data = yaml.safe_load(file.read_text())
+                if data is None:  # Catch empty file
+                    data = {}
+            for cred in self._credentials:
+                data[cred.iri] = cred.dict(exclude={"iri"})
+            with open(fp, "w") as stream:
+                yaml.dump(data, stream)
+            if file_already_exists:
+                print(f"Credentials file updated at '{fp.resolve()}'.")
+            else:
+                print(f"Credentials file created at '{fp.resolve()}'.")
+
+        # Creating or updating .gitignore file in the working directory
+        cwd = Path.cwd()
+        potential_fp = [cwd / ".gitignore", cwd.parent / ".gitignore"]
+        write_to_fp = potential_fp[0]
+        for fp in potential_fp:
+            if fp.exists():
+                write_to_fp = fp
+                break
+        if not write_to_fp.exists():
+            if not write_to_fp.parent.exists():
+                write_to_fp.parent.mkdir(parents=True)
+            write_to_fp.touch()
+        with open(write_to_fp, "r") as stream:
+            content = stream.read()
+        comment_set = False
+        for ii, fp in enumerate(filepath_):
+            if fp.name not in content:
+                print(f"Adding '{fp.name}' to gitignore file '{write_to_fp}'.")
+                with open(write_to_fp, "a") as stream:
+                    if comment_set:
+                        stream.write(
+                            "\n# Automatically added by osw.auth.CredentialManager."
+                            "save_credentials_to_file:"
+                        )
+                        comment_set = True
+                    stream.write(f"\n*{fp.name}")
 
 
 CredentialManager.CredentialConfig.update_forward_refs()

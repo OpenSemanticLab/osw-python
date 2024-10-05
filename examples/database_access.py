8ef59495aa398930744ff9c8d38b6e4530ded504
SimonTaurus <simon.stier@gmx.de>
2024-10-05 19:13:30 +0200

    refactor: update examples
    remove outdated code/example

--- examples/database_access.py
+++ examples/database_access.py
@@ -2,7 +2,6 @@ import os
 
 import osw.model.entity as model
 from osw.auth import CredentialManager
-from osw.core import OSW
 from osw.express import OswExpress
 
 # Create/update the password file under examples/accounts.pwd.yaml
@@ -12,36 +11,14 @@ pwd_file_path = os.path.join(
 # pwd_file_path = "./accounts.pwd.yaml"
 osw_obj = OswExpress(domain="wiki-dev.open-semantic-lab.org", cred_fp=pwd_file_path)
 
-osw_obj.site.enable_cache()  # skip downloads on second request
-
-# osw_obj.fetch_schema() #this will load the current entity schema from the OSW instance. You may have to re-run the script to get the updated schema extension. Requires 'pip install datamodel-code-generator'
-if True:
-    osw_obj.fetch_schema(
-        OSW.FetchSchemaParam(
-            schema_title="Category:OSW02590972aeba46d7864ed492c0c11384",  # Host
-            mode="replace",
-        )
-    )
-    osw_obj.fetch_schema(
-        OSW.FetchSchemaParam(
-            schema_title="Category:OSWacdb001c926c46b998af3e645d36b13f",
-            # DatabaseServer
-            mode="append",
-        )
-    )
-    osw_obj.fetch_schema(
-        OSW.FetchSchemaParam(
-            schema_title="Category:OSW51ad0d1716254c77a2b7a03217f23b43",  # Database
-            mode="append",
-        )
-    )
-    osw_obj.fetch_schema(
-        OSW.FetchSchemaParam(
-            schema_title="Category:OSW33c7c3a4076a4a58b20d818c162e84e6",  # DatabaseType
-            mode="append",
-        )
-    )
-
+# load dependencies
+DEPENDENCIES = {
+    "Host": "Category:OSW02590972aeba46d7864ed492c0c11384",
+    "DatabaseServer": "Category:OSWacdb001c926c46b998af3e645d36b13f",
+    "Database": "Category:OSW51ad0d1716254c77a2b7a03217f23b43",
+    "DatabaseType": "Category:OSW33c7c3a4076a4a58b20d818c162e84e6",
+}
+osw_obj.install_dependencies(DEPENDENCIES, mode="append")
 
 # Make sure to import controllers after updating the model (ignore linter warning)
 import osw.controller as controller  # noqa: E402

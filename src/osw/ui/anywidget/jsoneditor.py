fe29ae94a0e314c97cbb6319adfadd4ae48cb6bd
SimonTaurus <simon.stier@gmx.de>
2025-01-07 06:54:12 +0100

    fead: add anywidget implementation

---
+++ src/osw/ui/anywidget/jsoneditor.py
@@ -0,0 +1,29 @@
+import os
+from pathlib import Path
+
+import anywidget
+import traitlets
+
+bundled_assets_dir = Path(os.path.abspath("")).parent / "vue" / "dist" / "anywidget"
+
+
+class JsonEditorWidget(anywidget.AnyWidget):
+    # _esm = "jsoneditor.mjs" # works
+    # _css =  'https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css'
+    #   works but conflict with vscode
+    # _esm = str(bundled_assets_dir / "jsoneditor_vue.mjs")
+    # we need to prefix this static import since the dynamic import
+    # import "https://esm.sh/@json-editor/json-editor@latest" does not work
+    _esm = (
+        'import {JSONEditor} from "https://esm.sh/@json-editor/json-editor@latest"\n'
+        + (bundled_assets_dir / "jsoneditor_vue.mjs").read_text()
+    )
+    # scoped css does not conflict with vscode styling
+    _css = str(bundled_assets_dir / "jsoneditor_vue.css")  # .read_text()
+
+    value = traitlets.Dict({}).tag(sync=True)
+
+    @traitlets.observe("value")
+    def _observe_value(self, change):
+        print(change["old"])
+        print(change["new"])

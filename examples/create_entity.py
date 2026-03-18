05ecab5f6a68ac664a731e500856e2873cd111b7
Lukas Gold <lukas.gold@isc.fraunhofer.de>
2026-03-18 16:07:39 +0100

    Some (incomplete) fixes to examples

--- examples/create_entity.py
+++ examples/create_entity.py
@@ -3,7 +3,8 @@ from pprint import pprint
 import osw.model.entity as model
 
 my_entity = model.Item(
-    label=[model.Label(text="MyItem")], statements=[model.Statement(predicate="IsA")]
+    label=[model.Label(text="MyItem")],
+    statements=[model.DataStatement(property="IsA", value="Category:Item")],
 )
 pprint(my_entity.dict())
 print(my_entity.json())

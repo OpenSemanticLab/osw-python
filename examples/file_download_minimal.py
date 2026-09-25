3caf72bfa86b68c2e1a6a70b223c1ed729b08bbf
Matthias Albert Popp <matthias.albert.popp@isc.fraunhofer.de>
2026-09-25 18:50:17 +0200

    add inter_osw_copy_with_images_attachments.py

--- examples/file_download_minimal.py
+++ examples/file_download_minimal.py
@@ -3,7 +3,7 @@ from osw.express import osw_download_file
 # Download a file from an OSW instance and save it to a local file
 local_file = osw_download_file(
     "https://wiki-dev.open-semantic-lab.org/wiki/"
-    "File:OSWaa635a571dfb4aa682e43b98937f5dd3.pdf"
+    "File:OSW3bbf46a9a271409bb6d75728df318a95.png"
     # , use_cached=True
     # , overwrite=True
 )
